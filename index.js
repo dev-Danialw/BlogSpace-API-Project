1a9aa9d23b761f7919f36cacf6f25db6ca42acf3
Danial-Waheed <daniyalwahied@gmail.com>
2022-03-06 21:51:13 +0500

    New post functionality

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-  .then((res) => res.json())
-  .then((data) => {
-    const postsArr = data.slice(0, 5);
-    let html = "";
-    for (let post of postsArr) {
-      html += `
+let postsArray = [];
+
+function renderPosts() {
+  let html = "";
+  for (let post of postsArray) {
+    html += `
         <h3>${post.title}</h3>
         <p>${post.body}</p>
         <hr/>
         `;
-    }
-    document.getElementById("blog-list").innerHTML = html;
+  }
+  document.getElementById("blog-list").innerHTML = html;
+}
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    postsArray = data.slice(0, 5);
+    renderPosts();
   });
 
 document.getElementById("new-post").addEventListener("submit", (e) => {
@@ -21,5 +27,18 @@ document.getElementById("new-post").addEventListener("submit", (e) => {
     title: postTitle,
     body: postBody,
   };
-  console.log(data);
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+    .then((res) => res.json())
+    .then((post) => {
+      postsArray.unshift(post);
+      renderPosts();
+    });
 });
